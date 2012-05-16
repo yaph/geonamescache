03f271d83415fa2085fb356dd587b8a54d919442
Ramiro Gomez <web@ramiro.org>
2012-05-16 14:46:44 +0200

    add scripts

---
+++ scripts/us_states.js
@@ -0,0 +1,13 @@
+// grab info from http://www.geonames.org/US/administrative-division-united-states.html
+$('#subdivtable1 tr').each(function(idx, elt){
+    var code = $(elt).find('span[id*="isoSpan"]').text();
+    if (code) {
+        var link = $(elt).find('span[id*="nameSpan"] a');
+        var href = link.attr('href');
+        if (href) {
+            var gid = href.split('/')[3];
+            var name = link.text();
+            console.log("'"+code+"': {'name': '"+name+"', 'geonameid': "+gid+"},");
+        }
+    }
+});
