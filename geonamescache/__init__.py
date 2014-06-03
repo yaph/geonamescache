fcc662ad287a789f51a44bf7be13069ac27a4af2
Ramiro Gómez <code@ramiro.org>
2014-06-03 12:58:21 +0200

    fix #2: added mappings module with country name mappings

--- geonamescache/__init__.py
+++ geonamescache/__init__.py
@@ -9,7 +9,7 @@ geonamescache
 """
 
 __title__ = 'geonamescache'
-__version__ = '0.11'
+__version__ = '0.12'
 __author__ = 'Ramiro Gómez'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Ramiro Gómez'
