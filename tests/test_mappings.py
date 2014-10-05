8bec9a465b169648e9af2509835517a2f660f56e
Ramiro Gómez <code@ramiro.org>
2014-10-05 17:49:34 +0200

    added mappings and tests for mappings

---
+++ tests/test_mappings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import unittest
+from ..geonamescache import mappings
+
+
+class MappingsTestSuite(unittest.TestCase):
+    """GeonamesCache Mappings test cases."""
+
+    # Test a few mappings to make sure mappings work.
+    def test_mappings(self):
+        self.assertEquals(mappings.country_names['Macau'], 'Macao')
+        self.assertEquals(mappings.country_names['Pitcairn Islands'], 'Pitcairn')
+        self.assertEquals(mappings.country_names['Saint Barthélemy'], 'Saint Barthelemy')
+
+
+if __name__ == '__main__':
+    unittest.main()
