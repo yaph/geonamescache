bc14b0e82e8d9a83a94daf89786f0a1d7a1c1ecc
Ramiro Gómez <code@ramiro.org>
2014-10-05 17:49:34 +0200

    added mappings and tests for mappings

--- geonamescache/__init__.py
+++ geonamescache/__init__.py
@@ -9,7 +9,7 @@ geonamescache
 """
 
 __title__ = 'geonamescache'
-__version__ = '0.14'
+__version__ = '0.15'
 __author__ = 'Ramiro Gómez'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Ramiro Gómez'
