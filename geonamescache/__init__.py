2eefc9010b001aa025bcca4578747886d33fdede
Ramiro Gómez <code@ramiro.org>
2016-09-25 22:04:59 +0200

    version and year bump

--- geonamescache/__init__.py
+++ geonamescache/__init__.py
@@ -3,13 +3,13 @@
 geonamescache
 =============
 
-:copyright: (c) 2015 by Ramiro Gómez.
+:copyright: (c) 2016 by Ramiro Gómez.
 :license: ISC, see LICENSE for more details.
 
 """
 
 __title__ = 'geonamescache'
-__version__ = '0.3.0'
+__version__ = '0.3.0    '
 __author__ = 'Ramiro Gómez'
 __license__ = 'MIT'
 
