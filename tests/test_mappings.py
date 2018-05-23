fc054dbf9651dc54bd9785ea1662ad50577175ea
Ramiro Gómez <code@ramiro.org>
2018-05-24 00:09:49 +0200

    Add records to mappings, add mapper tests

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from geonamescache import mappings
+from geonamescache.mappers import country
 
 
 class MappingsTestSuite(unittest.TestCase):
@@ -11,6 +12,13 @@ class MappingsTestSuite(unittest.TestCase):
         self.assertEquals(mappings.country_names['Macau'], 'Macao')
         self.assertEquals(mappings.country_names['Pitcairn Islands'], 'Pitcairn')
         self.assertEquals(mappings.country_names['Saint Barthélemy'], 'Saint Barthelemy')
+
+    def test_country_name_iso3_mapper(self):
+        mapper = country(from_key='name', to_key='iso3')
+        self.assertEquals(mapper('Burma'), 'MMR')
+        self.assertEquals(mapper('South Korea'), 'KOR')
+        self.assertEquals(mapper('The Netherlands'), 'NLD')
+        self.assertEquals(mapper('USA'), 'USA')
 
 
 if __name__ == '__main__':
