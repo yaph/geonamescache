5f671ce164937000b8de3c94648cf40d3183955f
Ramiro Gómez <code@ramiro.org>
2021-11-02 21:49:46 +0100

    Add base and dev requirements Upgrade dependencies Replace assertEquals with assertEqual

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -9,16 +9,16 @@ class MappingsTestSuite(unittest.TestCase):
 
     # Test a few mappings to make sure mappings work.
     def test_mappings(self):
-        self.assertEquals(mappings.country_names['Macau'], 'Macao')
-        self.assertEquals(mappings.country_names['Pitcairn Islands'], 'Pitcairn')
-        self.assertEquals(mappings.country_names['Saint Barthélemy'], 'Saint Barthelemy')
+        self.assertEqual(mappings.country_names['Macau'], 'Macao')
+        self.assertEqual(mappings.country_names['Pitcairn Islands'], 'Pitcairn')
+        self.assertEqual(mappings.country_names['Saint Barthélemy'], 'Saint Barthelemy')
 
     def test_country_name_iso3_mapper(self):
         mapper = country(from_key='name', to_key='iso3')
-        self.assertEquals(mapper('Burma'), 'MMR')
-        self.assertEquals(mapper('South Korea'), 'KOR')
-        self.assertEquals(mapper('The Netherlands'), 'NLD')
-        self.assertEquals(mapper('USA'), 'USA')
+        self.assertEqual(mapper('Burma'), 'MMR')
+        self.assertEqual(mapper('South Korea'), 'KOR')
+        self.assertEqual(mapper('The Netherlands'), 'NLD')
+        self.assertEqual(mapper('USA'), 'USA')
 
 
 if __name__ == '__main__':
