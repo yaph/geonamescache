69662ef6670a77291359f0f7c29d27502f407403
Ramiro Gómez <code@ramiro.org>
2021-11-02 22:59:52 +0100

    Remove Python 2.7 support

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
