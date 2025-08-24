9d04d7b37c3a1b235733690cdbb0a00be2db8bdd
connortodd21 <connortodd21@gmail.com>
2025-08-23 17:26:56 -0700

    Use new 2020 counties list for us_counties

--- bin/us_counties.py
+++ bin/us_counties.py
@@ -7,6 +7,20 @@ p_data = Path('data')
 
 reader = csv.reader(p_data.joinpath('us_counties.txt').open())
 
-counties = [{'fips': line[1] + line[2], 'name': line[3], 'state': line[0]} for line in reader]
+state_name_idx = 0
+state_fips_idx = 1
+county_fips_idx = 2
+county_name_idx = 4
+
+counties = []
+for line in reader: 
+    current_line = line[0].split("|")
+    counties.append(
+        {
+            'fips': current_line[state_fips_idx] + current_line[county_fips_idx], 
+            'name': current_line[county_name_idx], 
+            'state': current_line[state_name_idx]
+        }
+    )
 
 p_data.joinpath('us_counties.json').write_text(json.dumps(counties))
