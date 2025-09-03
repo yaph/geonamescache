25527f4c7de0353e5448b46e4486db68a2536914
Ramiro Gómez <code@ramiro.org>
2025-09-03 23:50:14 +0200

    Move some tests to test_data.py Replace test suite class with simple test functions

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,19 +1,68 @@
 from geonamescache import GeonamesCache
 
-geonamescache = GeonamesCache()
+gc = GeonamesCache()
 
 
 def test_cities_len():
     # Make sure there are more than 25000 cities
-    assert len(geonamescache.get_cities()) > 25000
+    assert len(gc.get_cities()) > 25000
 
 
 def test_countries_len():
     # Make sure there are more than 250 countries
-    assert len(geonamescache.get_countries()) > 250
+    assert len(gc.get_countries()) > 250
 
 
 def test_us_counties_len():
     # Make sure there are more than 3000 counties
-    us_counties = geonamescache.get_us_counties()
+    us_counties = gc.get_us_counties()
     assert len(us_counties) > 3000
+
+
+def test_continents():
+    continents = gc.get_continents()
+    testdata = (
+        ('AF', 'Africa'),
+        ('AN', 'Antarctica'),
+        ('AS', 'Asia'),
+        ('EU', 'Europe'),
+        ('NA', 'North America'),
+        ('OC', 'Oceania'),
+        ('SA', 'South America'),
+    )
+    for code, name in testdata:
+        assert code in continents
+        assert name == continents[code]['name']
+
+    for code in ['XX', 'OO']:
+        assert code not in continents
+
+
+def test_countries():
+    countries = gc.get_countries()
+    testdata = (('ES', 'Spain'), ('FR', 'France'), ('US', 'United States'))
+    for code, name in testdata:
+        assert code in countries
+        assert name == countries[code]['name']
+
+    for code in ['XX', 'OO']:
+        assert code not in countries
+
+
+def test_us_counties_fips():
+    # Ensure correct mapping
+    us_counties = {c['fips']: c for c in gc.get_us_counties()}
+    assert us_counties['01001']['name'] == 'Autauga County'
+    assert us_counties['06037']['name'] == 'Los Angeles County'
+
+
+def test_us_states():
+    us_states = gc.get_us_states()
+
+    testdata = (('NM', 'New Mexico'), ('CA', 'California'), ('NV', 'Nevada'))
+    for code, name in testdata:
+        assert code in us_states
+        assert name == us_states[code]['name']
+
+    for code in ['XX', 'OO']:
+        assert code not in us_states
--- tests/test_geonamescache.py
+++ tests/test_geonamescache.py
@@ -1,105 +1,70 @@
 from geonamescache import GeonamesCache
 
+gc = GeonamesCache()
 
-class TestGeonamesCacheSuite:
-    """GeonamesCache test cases."""
 
-    def setup_method(self):
-        self.geonamescache = GeonamesCache()
+def test_get_countries_by_names():
+    # Length of get_countries_by_names dict and get_countries dict must be
+    # the same, unless country names wouldn't be unique.
+    assert len(gc.get_countries_by_names()), len(gc.get_countries())
 
-    def test_continents(self):
-        continents = self.geonamescache.get_continents()
-        testdata = (
-            ('AF', 'Africa'),
-            ('AN', 'Antarctica'),
-            ('AS', 'Asia'),
-            ('EU', 'Europe'),
-            ('NA', 'North America'),
-            ('OC', 'Oceania'),
-            ('SA', 'South America'),
-        )
-        for code, name in testdata:
-            assert code in continents
-            assert name == continents[code]['name']
 
-        for code in ['XX', 'OO']:
-            assert code not in continents
+def test_get_cities_by_name():
+    cities = gc.get_cities()
+    for gid, name in (('3191316', 'Samobor'), ('3107112', 'Rivas-Vaciamadrid')):
+        assert name == cities[gid]['name']
 
-    def test_get_countries(self):
-        countries = self.geonamescache.get_countries()
 
-        testdata = (('ES', 'Spain'), ('FR', 'France'), ('US', 'United States'))
-        for code, name in testdata:
-            assert code in countries
-            assert name == countries[code]['name']
+def test_get_cities_by_name_madrid():
+    assert len(gc.get_cities_by_name('Madrid')) > 1
 
-        for code in ['XX', 'OO']:
-            assert code not in countries
 
-    def test_us_states(self):
-        us_states = self.geonamescache.get_us_states()
+def test_cities_in_us_states():
+    cities = gc.get_cities()
+    for gid, name, us_state in (('4164138', 'Miami', 'FL'), ('4525353', 'Springfield', 'OH')):
+        assert name == cities[gid]['name']
+        assert us_state == cities[gid]['admin1code']
 
-        testdata = (('NM', 'New Mexico'), ('CA', 'California'), ('NV', 'Nevada'))
-        for code, name in testdata:
-            assert code in us_states
-            assert name == us_states[code]['name']
 
-        for code in ['XX', 'OO']:
-            assert code not in us_states
+def test_search_cities():
+    cities = gc.search_cities('Kiev')
+    assert len(cities) >= 1
 
-    def test_get_countries_by_names(self):
-        # Length of get_countries_by_names dict and get_countries dict must be
-        # the same, unless country names wouldn't be unique.
-        assert len(self.geonamescache.get_countries_by_names()), len(self.geonamescache.get_countries())
 
-    def test_get_cities_by_name(self):
-        cities = self.geonamescache.get_cities()
-        for gid, name in (('3191316', 'Samobor'), ('3107112', 'Rivas-Vaciamadrid')):
-            assert name == cities[gid]['name']
+def test_search_cities_case_sensitive():
+    cities = gc.search_cities('Stoke-On-Trent', case_sensitive=True)
+    assert len(cities) == 0
+    cities = gc.search_cities('Stoke-On-Trent')
+    assert len(cities) == 1
 
-    def test_get_cities_by_name_madrid(self):
-        assert len(self.geonamescache.get_cities_by_name('Madrid')) > 1
 
-    def test_cities_in_us_states(self):
-        cities = self.geonamescache.get_cities()
-        for gid, name, us_state in (('4164138', 'Miami', 'FL'), ('4525353', 'Springfield', 'OH')):
-            assert name == cities[gid]['name']
-            assert us_state == cities[gid]['admin1code']
+def test_search_cities_alternatenames_contains_search():
+    assert (
+        len(gc.search_cities('London'))
+        > len(gc.search_cities('London', contains_search=False))
+        > 1
+    )
 
-    def test_search_cities(self):
-        cities = self.geonamescache.search_cities('Kiev')
-        assert len(cities) >= 1
 
-    def test_search_cities_case_sensitive(self):
-        cities = self.geonamescache.search_cities('Stoke-On-Trent', case_sensitive=True)
-        assert len(cities) == 0
-        cities = self.geonamescache.search_cities('Stoke-On-Trent')
-        assert len(cities) == 1
+def test_search_cities_name_contains_search():
+    assert (
+        len(gc.search_cities('London', 'name'))
+        > len(gc.search_cities('London', 'name', contains_search=False))
+        > 1
+    )
 
-    def test_search_cities_alternatenames_contains_search(self):
-        assert (
-            len(self.geonamescache.search_cities('London'))
-            > len(self.geonamescache.search_cities('London', contains_search=False))
-            > 1
-        )
 
-    def test_search_cities_name_contains_search(self):
-        assert (
-            len(self.geonamescache.search_cities('London', 'name'))
-            > len(self.geonamescache.search_cities('London', 'name', contains_search=False))
-            > 1
-        )
+def test_search_cities_alternatenames_contains_search_and_case_sensitive():
+    assert (
+        len(gc.search_cities('London', case_sensitive=True))
+        > len(gc.search_cities('London', case_sensitive=True, contains_search=False))
+        > 1
+    )
 
-    def test_search_cities_alternatenames_contains_search_and_case_sensitive(self):
-        assert (
-            len(self.geonamescache.search_cities('London', case_sensitive=True))
-            > len(self.geonamescache.search_cities('London', case_sensitive=True, contains_search=False))
-            > 1
-        )
 
-    def test_search_cities_name_contains_search_and_case_sensitive(self):
-        assert (
-            len(self.geonamescache.search_cities('London', 'name', case_sensitive=True))
-            > len(self.geonamescache.search_cities('London', 'name', case_sensitive=True, contains_search=False))
-            > 1
-        )
+def test_search_cities_name_contains_search_and_case_sensitive():
+    assert (
+        len(gc.search_cities('London', 'name', case_sensitive=True))
+        > len(gc.search_cities('London', 'name', case_sensitive=True, contains_search=False))
+        > 1
+    )
